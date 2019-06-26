81abce516e4b94baac06fa024e679afad7389193
Franklin Sarpong Boampong <frankboamps@fb.com>
2019-06-26 11:04:15 -0700

    Initial Commit

---
+++ flixster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flixster
+//
+//  Created by frankboamps on 6/26/19.
+//  Copyright © 2019 frankboamps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ flixster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  flixster
+//
+//  Created by frankboamps on 6/26/19.
+//  Copyright © 2019 frankboamps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
