1e606bbef0dd893c27f860f3a914b8420b553de4
Franklin Sarpong Boampong <frankboamps@fb.com>
2019-06-27 10:29:11 -0700

    Implemented second page

---
+++ flixster/MovieCell.h
@@ -0,0 +1,21 @@
+//
+//  MovieCell.h
+//  flixster
+//
+//  Created by frankboamps on 6/26/19.
+//  Copyright © 2019 frankboamps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
